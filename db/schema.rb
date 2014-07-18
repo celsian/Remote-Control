aeca031d86326752df37709aae9907ebccc62052
Lucas Paolella <celsian@gmail.com>
2014-07-18 15:25:59 -0700

    Rails adds

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714183646) do
+ActiveRecord::Schema.define(version: 20140718221022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: true do |t|
+    t.string   "info"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "remote_controls", force: true do |t|
     t.string   "gpio"
