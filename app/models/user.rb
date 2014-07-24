f87482221d3b66e7e3a542ea9dc0ad460435aaf6
Lucas Paolella <celsian@gmail.com>
2014-07-24 12:08:45 -0700

    default scope set to email and added search function

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  default_scope order("email")
+
+  def self.search query
+    query = query.downcase
+    where("email LIKE :query", query: "%#{query}%")
+  end
+
 end
