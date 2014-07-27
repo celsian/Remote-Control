0ecaf2767c068778fded2b99929f68c778c79fc8
Lucas Paolella <celsian@gmail.com>
2014-07-26 20:01:54 -0700

    Added additional method call for different trigger type

--- app/models/note.rb
+++ app/models/note.rb
@@ -6,6 +6,11 @@ class Note < ActiveRecord::Base
     return Note.create(info: "#{current_user.email} triggered the #{remote_control.name}")
   end
 
+  def self.head_add(current_user, remote_control)
+    Note.check_count
+    return Note.create(info: "#{current_user.email} triggered the #{remote_control.name} for head level.")
+  end
+
   def self.check_count
     if Note.count > 99
       Note.last.destroy
