af72c85ed3b388b4e207310211716151b98ebf5d
Lucas Paolella <celsian@gmail.com>
2014-07-30 14:39:35 -0700

    Created factory for remote_control

--- spec/factories/remote_controls.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :remote_control do
-    gpio "MyString"
-    name "MyString"
-  end
-end
---
+++ spec/factories/remote_controls_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :remote_control do
+    gpio "21"
+    name  "gate"
+  end
+end
