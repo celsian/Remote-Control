587d093fbdc9c2004cd42e69af9ddcb56da16e7c
Lucas Paolella <celsian@gmail.com>
2014-07-30 14:35:00 -0700

    Added validations for remote_control model

--- app/models/remote_control.rb
+++ app/models/remote_control.rb
@@ -1,3 +1,16 @@
 class RemoteControl < ActiveRecord::Base
-  default_scope order("name")
+  default_scope { order("name") }
+
+  validates :gpio, presence: true, uniqueness: true
+  validate :gpio_validation
+
+  VALID_GPIO = ["4","17","18","21","22","23","24","25"]
+
+  def gpio_validation
+    gpios = ["4","17","18","21","22","23","24","25"]
+    unless gpios.include?(gpio)
+      errors.add(:base, 'value cannot be outside the valid GPIO Range.')
+    end
+  end
+
 end
