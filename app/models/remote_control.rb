291d036d7d9f3770aef92f67d419931eec2fa6f1
Lucas Paolella <celsian@gmail.com>
2014-07-30 14:38:11 -0700

    Removed unnecessary lines and reverted back to static array

--- app/models/remote_control.rb
+++ app/models/remote_control.rb
@@ -7,8 +7,7 @@ class RemoteControl < ActiveRecord::Base
   VALID_GPIO = ["4","17","18","21","22","23","24","25"]
 
   def gpio_validation
-    gpios = ["4","17","18","21","22","23","24","25"]
-    unless gpios.include?(gpio)
+    unless VALID_GPIO.include?(gpio)
       errors.add(:base, 'value cannot be outside the valid GPIO Range.')
     end
   end
