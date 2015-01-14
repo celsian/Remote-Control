9ee438b62ecae3bf858e9390636c6c37b4dfd755
Lucas Paolella <celsian@gmail.com>
2015-01-14 14:50:36 -0800

    Adding user reference
    Also added centralized NOTE_COUNT variable for sanity.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,21 +1,24 @@
 class Note < ActiveRecord::Base
   belongs_to :remote_control
+  belongs_to :user
+
+  NOTE_COUNT = 100
 
   default_scope { order("id DESC") }
 
   def self.add(current_user, remote_control)
     Note.check_count
-    return Note.create(info: "#{current_user.email} triggered the #{remote_control.name}", remote_control: remote_control)
+    return Note.create(info: "#{current_user.email} triggered the #{remote_control.name}", remote_control: remote_control, user: current_user)
   end
 
   def self.head_add(current_user, remote_control)
     Note.check_count
-    return Note.create(info: "#{current_user.email} triggered the #{remote_control.name} for head level.", remote_control: remote_control)
+    return Note.create(info: "#{current_user.email} triggered the #{remote_control.name} for head level.", remote_control: remote_control, user: current_user)
   end
 
   def self.check_count
-    if Note.count > 99
-      overflow = Note.all.offset(99)
+    if Note.count > (NOTE_COUNT-1)
+      overflow = Note.all.offset((NOTE_COUNT-1))
       overflow.each { |note| note.destroy }
     end
   end
