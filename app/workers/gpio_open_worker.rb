8d78176dfe45ef68366c5270263e7e74b448c159
Lucas Paolella <celsian@gmail.com>
2016-05-02 20:25:58 -0700

    update gems, form-control for devise

--- app/workers/gpio_open_worker.rb
+++ app/workers/gpio_open_worker.rb
@@ -1,14 +1,14 @@
 class GpioOpenWorker
   include Sidekiq::Worker
   require 'rubygems'
-  require 'pi_piper'
+  # require 'pi_piper'
 
   def perform(remote_control_id)
     remote_control = RemoteControl.find(remote_control_id)
     
-    pin = PiPiper::Pin.new(:pin => remote_control.gpio, :direction => :out)
+    # pin = PiPiper::Pin.new(:pin => remote_control.gpio, :direction => :out)
     sleep 1
-    pin.on
+    # pin.on
   end
   
 end
