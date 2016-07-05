baa183183e3a50717109602e649b1794fbacd38e
Lucas Paolella <celsian@gmail.com>
2016-07-05 11:58:15 -0700

    Added jquery turbolinks and boostrap-sprockets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
+//= require bootstrap-sprockets
 //= require jquery_ujs
-//= require bootstrap
-//= require turbolinks
+// require turbolinks
 //= require_tree .
-
-Turbolinks.enableTransitionCache();
