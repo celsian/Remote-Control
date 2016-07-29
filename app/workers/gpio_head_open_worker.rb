2f65ccfb6e209199d711e9a68a4d0a720d3629d5
Lucas Paolella <celsian@gmail.com>
2016-07-29 01:33:00 -0700

    Traveling too far, adjusting down 0.5

--- app/workers/gpio_head_open_worker.rb
+++ app/workers/gpio_head_open_worker.rb
@@ -2,7 +2,7 @@ class GpioHeadOpenWorker
   include Sidekiq::Worker
   require 'rubygems'
 
-  TIME_IT_TAKES_DOOR_TO_TRAVEL_TO_HEAD_LEVEL = 11
+  TIME_IT_TAKES_DOOR_TO_TRAVEL_TO_HEAD_LEVEL = 10.5
 
   def perform(remote_control_id)
     GpioOpenWorker.perform_async(remote_control_id)
