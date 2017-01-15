8e9e0a7e36bfd524a20dc09af4c439154effa157
Lucas Paolella <celsian@gmail.com>
2017-01-14 18:51:54 -0800

    Change head level wait time.
    It would appear the door moves slower during winter, I'm not really sure.

--- app/workers/gpio_head_open_worker.rb
+++ app/workers/gpio_head_open_worker.rb
@@ -2,8 +2,8 @@ class GpioHeadOpenWorker
   include Sidekiq::Worker
   require 'rubygems'
 
-  TIME_IT_TAKES_DOOR_TO_TRAVEL_TO_HEAD_LEVEL = 10.5
-
+  TIME_IT_TAKES_DOOR_TO_TRAVEL_TO_HEAD_LEVEL = 11.5
+  
   def perform(remote_control_id)
     GpioOpenWorker.perform_async(remote_control_id)
     sleep TIME_IT_TAKES_DOOR_TO_TRAVEL_TO_HEAD_LEVEL
