9282d01ee9d815e025cdb2d5f5fe1cc945470a5e
Lucas Paolella <lpaolella@nav.com>
2022-12-11 09:34:12 -0800

    FactoryGirl to FactoryBot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,7 +41,7 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Warden::Test::Helpers
