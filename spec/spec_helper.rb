4359ae37304b888e448c5c7a693b5f86d0e2d607
Lucas Paolella <lpaolella@nav.com>
2022-12-11 23:44:14 -0800

    Require factory_bot

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'rubygems'
-require 'factory_bot_rails'
+require 'factory_bot'
 require_relative 'support/controller_helpers'
 require 'devise'
 
