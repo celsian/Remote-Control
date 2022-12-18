2698616d7029757832a4d871a8b8353ec86728a4
Lucas Paolella <lpaolella@nav.com>
2022-12-18 12:15:41 -0800

    Adjust factory and test public method update

--- spec/models/api_key_spec.rb
+++ spec/models/api_key_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ApiKey, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:cu) { create(:controller_user) }
+  let(:apikey) { create(:api_key, user: cu) }
+
+  it "update_token" do
+    old_token = apikey.token
+
+    apikey.update_token
+
+    expect(apikey.token).to_not eq(old_token)
+  end
 end
