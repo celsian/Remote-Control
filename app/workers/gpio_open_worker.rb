7d852ca0fe7bad6d95cffbcf6ed8c5abddf1b336
LPaolella <lpaolella@nav.com>
2024-02-10 13:46:26 -0800

    Change pi_piper to pigpio

--- app/workers/gpio_open_worker.rb
+++ app/workers/gpio_open_worker.rb
@@ -5,15 +5,25 @@ class GpioOpenWorker
 #  if Rails.env.production?
 #    require 'pi_piper'
 #  end
+  if Rails.env.production?
+    include Pigpio::Constant
+  end
 
   def perform(remote_control_id)
     remote_control = RemoteControl.find(remote_control_id)
-    
-    if Rails.env.production?
 
+    if Rails.env.production?
+      pi = Pigpio.new
+
+      gpio = pi.gpio(remote_control.gpio)
+      gpio.mode = PI_OUTPUT
+      gpio.pud = PI_PUD_OFF
+
+      gpio.write 1
+      sleep 1.0
+      gpio.write 0
     end
   end
-
 #  def perform(remote_control_id)
 #    remote_control = RemoteControl.find(remote_control_id)
 #
@@ -25,5 +35,4 @@ class GpioOpenWorker
 #      File.open("/sys/class/gpio/unexport","w") { |f| f.write(pin.pin) }
 #    end
 #  end
-
 end
