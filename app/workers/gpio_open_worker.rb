b27096eedae262d8acbf0dc660b6af70ba515f98
Celsian <celsian@gmail.com>
2024-02-10 13:32:30 -0800

    Begin changing pi_piper to pigpio

--- app/workers/gpio_open_worker.rb
+++ app/workers/gpio_open_worker.rb
@@ -2,20 +2,28 @@ class GpioOpenWorker
   include Sidekiq::Worker
   require 'rubygems'
 
-  if Rails.env.production?
-    require 'pi_piper'
-  end
+#  if Rails.env.production?
+#    require 'pi_piper'
+#  end
 
   def perform(remote_control_id)
     remote_control = RemoteControl.find(remote_control_id)
-
+    
     if Rails.env.production?
-      pin = PiPiper::Pin.new(:pin => remote_control.gpio, :direction => :out)
-      sleep 1.0
-      pin.on
 
-      File.open("/sys/class/gpio/unexport","w") { |f| f.write(pin.pin) }
     end
   end
+
+#  def perform(remote_control_id)
+#    remote_control = RemoteControl.find(remote_control_id)
+#
+#    if Rails.env.production?
+#      pin = PiPiper::Pin.new(:pin => remote_control.gpio, :direction => :out)
+#      sleep 1.0
+#      pin.on
+#
+#      File.open("/sys/class/gpio/unexport","w") { |f| f.write(pin.pin) }
+#    end
+#  end
 
 end
