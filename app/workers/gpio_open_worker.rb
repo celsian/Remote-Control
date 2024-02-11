ba790a12d2e86ea37975735ea4733cce6b3711f7
Celsian <celsian@gmail.com>
2024-02-10 19:01:43 -0800

    RemoteControl.gpio is a string, but the pi.gpio(X) requires an integer. Removed pi_piper code

--- app/workers/gpio_open_worker.rb
+++ app/workers/gpio_open_worker.rb
@@ -1,10 +1,7 @@
 class GpioOpenWorker
-  include Sidekiq::Worker
+  include Sidekiq::Job
   require 'rubygems'
 
-#  if Rails.env.production?
-#    require 'pi_piper'
-#  end
   if Rails.env.production?
     include Pigpio::Constant
   end
@@ -15,24 +12,15 @@ class GpioOpenWorker
     if Rails.env.production?
       pi = Pigpio.new
 
-      gpio = pi.gpio(remote_control.gpio)
+      gpio = pi.gpio(remote_control.gpio.to_i)
       gpio.mode = PI_OUTPUT
       gpio.pud = PI_PUD_OFF
 
       gpio.write 1
       sleep 1.0
       gpio.write 0
+
+      pi.stop
     end
   end
-#  def perform(remote_control_id)
-#    remote_control = RemoteControl.find(remote_control_id)
-#
-#    if Rails.env.production?
-#      pin = PiPiper::Pin.new(:pin => remote_control.gpio, :direction => :out)
-#      sleep 1.0
-#      pin.on
-#
-#      File.open("/sys/class/gpio/unexport","w") { |f| f.write(pin.pin) }
-#    end
-#  end
 end
