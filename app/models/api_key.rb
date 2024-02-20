59a0924c03617f5c63bc37fb484540526f319257
LPaolella <lpaolella@nav.com>
2024-02-19 18:47:53 -0800

    Re-add blind_index. It's necessary for the way we search for API Keys.

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -2,15 +2,20 @@ class ApiKey < ApplicationRecord
   belongs_to :user
   has_encrypted :token
   # blind_index :token, slow: true
+  blind_index :token
 
   before_create :set_token
 
-  # validates :token, uniqueness: true
+  validates :token, uniqueness: true
   validates :user, uniqueness: true, presence: true
 
   def update_token
     self.token = secure_string
     self.save
+  end
+
+  def self.get_tokens
+    ApiKey.find_by(token: "6cb6a9efbaec802cbe2ad6f6a72ad176").id
   end
 
   private
